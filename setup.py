2445f2e0cec391af0edb0e1372fa0fd8b6b7ef85
zbo <ilovenwd@163.com>
2014-06-21 19:05:25 +0800

    init

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+#http://docs.python.org/2/distutils/setupscript.html
+
+setup(
+    name='heroics',
+    version='0.0.1',
+
+    author='dev',
+    author_email='dev@agutong.com',
+    url='http://git.agutong.com/dev/heroics',
+
+    license='LICENSE',
+    description='heroics',
+    long_description=open('README.md').read(),
+
+    packages=[
+      'heroics',
+    ],
+
+    package_data = {
+        #'heroics': ['config/*.yml'],
+    },
+
+    data_files=[
+        #('/etc/init.d', ['bin/init-heroics'])
+    ],
+
+    scripts=[
+        'bin/heroics'
+    ],
+
+    install_requires=[
+        #"Django >= 1.1.1",
+    ],
+
+    dependency_links=[
+        #zip/tar urls
+    ]
+)
