5d1a8fde226c61ebb680d80e8b81d95fcea8d08c
zbo <zbo@agutong.com>
2014-07-18 23:52:55 +0800

    use kuankr_utils. fix resource name

--- heroics/cli.py
+++ heroics/cli.py
@@ -1,19 +1,6 @@
 import commandr
 
-from pyutils import log, debug, env_utils
-
-mod = 'heroics'
-env = env_utils.Env(mod)
-
-@commandr.command
-def run(url=None):
-    #TODO
-    print url
+from kuankr_utils import log, debug
 
 def main():
-    try:
-        commandr.Run()
-    except:
-        print debug.pretty_traceback()
-
-
+    pass
--- heroics/client.py
+++ heroics/client.py
@@ -6,32 +6,9 @@ import os
 import sys
 import json
 
-from kuankr_api_utils.client import Client as HttpClient
+from kuankr_utils.http_client import HttpClient
 
 from .resource import Resource
-
-def create_heroics_client(service, **options):
-    env = os.environ
-
-    url = env['%s_URI' % service.upper()]
-    schema = env['%s_SCHEMA' % service.upper()]
-    f = open(schema)
-    schema = json.loads(f.read())
-    f.close()
-
-    #schema = Heroics::Schema.new schema
-    headers = {}
-    for x in ['api_client', 'auth_token', 'admin_token']:
-        v = env.get('KUANKR_%s' % x.upper())
-        if v:
-            h = ('x_%s' % x).replace('_', '-')
-            headers[h] = v
-
-    dh = options.get('default_headers')
-    if dh:
-        headers.update(dh)
-    options['default_headers'] = headers
-    return Client(schema, url, options)
 
 class Client(object):
     def __init__(self, schema, url, options):
@@ -42,7 +19,8 @@ class Client(object):
 
         self._resources = {}
         for s in self._schema['properties']:
-            self._resources[s] = Resource(self, s)
+            res_name = s.replace('-','_')
+            self._resources[res_name] = Resource(self, s)
 
     def __getattr__(self, resource):
         return self._resources[resource]
